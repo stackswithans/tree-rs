6a738de9761251628de16fc686561bdcfb2e369f
Stacks-Mamba <stexor12@gmail.com>
2021-03-21 22:32:14 +0100

    Add first tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,21 +5,23 @@ use std::path::{Path, PathBuf};
 use std::ffi::OsStr;
 
 //Tag that indicates the kind of node (File or Dir)
-pub enum NodeKind{
+#[derive(Debug, PartialEq)]
+enum NodeKind{
     Dir,
     File
 }
 
 //Struct that represents a node in the file tree
-pub struct Node{
-    pub path : String,
-    pub kind : NodeKind,
-    pub depth : u64,
-    pub children : Option<Vec<Node>> //File nodes do not have children
+#[derive(Debug)]
+struct Node{
+     path : String,
+     kind : NodeKind,
+     depth : u64,
+     children : Option<Vec<Node>> //File nodes do not have children
 }
 
 impl Node{
-    pub fn new(path: String, kind : NodeKind, depth : u64) -> Node{
+    fn new(path: String, kind : NodeKind, depth : u64) -> Node{
         Node{
             children : match &kind{
                 NodeKind::Dir => Some(Vec::<Node>::new()), 
@@ -31,11 +33,11 @@ impl Node{
         }
     }
 
-    pub fn add_child(&mut self, child: Node){
+    fn add_child(&mut self, child: Node){
         self.children.as_mut().unwrap().push(child);
     }
 
-    pub fn print_node(&self){
+    fn print_node(&self){
         let mut depth = String::new();
         for i in 0..self.depth{
             depth.push_str("---");
@@ -50,6 +52,7 @@ impl Node{
 }
 
 //Struct that represents the directory tree
+#[derive(Debug)]
 struct Tree{
     root: Node
 }
@@ -69,7 +72,7 @@ impl Tree{
 
 }
 
-pub fn build_tree(parent: &mut Node, dir : &Path, depth : u64) -> io::Result<()>{
+fn build_tree(parent: &mut Node, dir : &Path, depth : u64) -> io::Result<()>{
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
         let path : PathBuf = entry.path();
@@ -81,7 +84,7 @@ pub fn build_tree(parent: &mut Node, dir : &Path, depth : u64) -> io::Result<()>
             .to_str()
             .unwrap()
         );
-        if path_str.starts_with("."){
+        if path_str.starts_with("."){ // Make this optional base on params
             continue;
         }
         if path.is_dir(){
@@ -110,4 +113,83 @@ pub fn run(dir : &Path) -> io::Result<()> {
     //Print directory tree
     tree.print_tree();
     Ok(())
+}
+
+#[cfg(test)]
+mod tests{
+    use std::fs::{DirBuilder, File};
+    use std::fs;
+    use super::Path;
+    use super::{Tree, Node, NodeKind};
+
+    #[test]
+    fn test_new_tree(){
+        let tree = Tree::new(String::from("foo/"));
+        assert_eq!(tree.root.path, "foo/");
+        assert_eq!(tree.root.children.unwrap().len(), 0);
+        assert_eq!(tree.root.kind, NodeKind::Dir);
+    }
+
+    #[test]
+    fn test_file_node_children(){
+        let node = Node::new(
+            String::from("foo.txt"),
+            NodeKind::File, 
+            1
+        );
+        assert_eq!(node.path, "foo.txt");
+        assert_eq!(node.depth, 1);
+        assert!(node.children.is_none());
+        assert_eq!(node.kind, NodeKind::File);
+    }
+
+    #[test]
+    fn test_build_tree(){
+        //Create test data
+        let builder = DirBuilder::new();
+        let root = Path::new("./_test");
+        builder.create(root).unwrap();
+        builder.create(root.join("foo")).unwrap();
+        builder.create(root.join("foo1")).unwrap();
+        File::create(root.join("foo.txt")).unwrap();
+        
+        let mut tree = Tree::new(
+            root
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap().to_string(),
+        );
+        tree.root.path.push_str("/");
+
+        super::build_tree(&mut tree.root, root, 0).unwrap();
+
+        assert_eq!(tree.root.path, "_test/");
+        assert_eq!(tree.root.kind, NodeKind::Dir);
+        assert_eq!(tree.root.children.as_ref().unwrap().len(), 3);
+
+        //Only testing this way because there are no guarantees about
+        //the sorting of the entries. There is probably a better to do this
+        for node in tree.root.children.as_ref().unwrap(){
+            match (node.path.as_str(), &node.kind) {
+                ("foo/", NodeKind::Dir) => {
+                    assert_eq!(
+                        node.children.as_ref()
+                        .unwrap().len(), 0
+                    )},
+                ("foo1/", NodeKind::Dir) => {
+                    assert_eq!(
+                        node.children.as_ref()
+                        .unwrap().len(), 0
+                    )},
+                ("foo.txt", NodeKind::File) => (),
+                _ => panic!("Bad child node found")
+            }
+        }
+        //Clean up test data
+        fs::remove_dir(root.join("foo")).unwrap();
+        fs::remove_dir(root.join("foo1")).unwrap();
+        fs::remove_file(root.join("foo.txt")).unwrap();
+        fs::remove_dir(root).unwrap();
+    }
 }
